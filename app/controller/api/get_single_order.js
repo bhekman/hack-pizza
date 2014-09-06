d04140f5532e577e3b4f240be66b0c1236789347
bhekman <bradley.hekman@gmail.com>
2014-09-06 14:21:33 -0400

    Added backend for /order/:key with placeholder ejs file.

--- app/controller/api/get_single_order.js
+++ app/controller/api/get_single_order.js
@@ -2,15 +2,14 @@ var mongoose = require('mongoose');
 var db_config = require('../../config/database.js');
 var orderSchema = require('../models/order.js');
 
-module.exports = function getOpenOrders(max_orders, view, res) {
+module.exports = function getOpenOrders(key, res) {
   var db = mongoose.createConnection(db_config.url);
   var Order = mongoose.model('Order', orderSchema);
 
-  Order.find({ status: 'created' },function(err,docs){
+  Order.findOne({ _id: key },function(err,docs){
       if (err)
           console.log('error occured in the database');
-      res.render(view, { orders : docs });
-      // TODO(bhekman): fix order limiting
-  })/*.limit(max_orders)*/;  
+      res.json(docs);
+  });
 }
 
