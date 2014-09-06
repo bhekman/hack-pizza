40e375c06efdd66211b71729d2a973bdd103fd14
bhekman <bradley.hekman@gmail.com>
2014-09-05 22:43:24 -0400

    Completed tutorial. Basic frame is ready.

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+  // TODO(bhekman): make this repo private....
+	'url' : 'mongodb://bhekman:scotch@proximus.modulusmongo.net:27017/eRyton2y' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
