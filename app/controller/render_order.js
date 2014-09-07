130418a25013e99999a6c5d21a008ce59e46c09b
bhekman <bradley.hekman@gmail.com>
2014-09-07 01:15:33 -0400

    Added is_owner & is_groupie info for order page.

--- app/controller/render_order.js
+++ app/controller/render_order.js
@@ -4,14 +4,21 @@ var mongoose = require('mongoose');
 var db_config = require('../../config/database.js');
 var orderSchema = require('../models/order.js');
 
-module.exports = function getOrder(user, key, view, res) {
+module.exports = function getOrder(user_email, key, view, res) {
   var db = mongoose.createConnection(db_config.url);
   var Order = mongoose.model('Order', orderSchema);
 
   Order.findOne({ _id: key },function(err,docs){
       if (err)
           console.log('error occured in the database');
-      console.log(docs);
-      res.render(view, { "order": docs, "user": user });
+
+      var is_owner = (user_email == docs.orderer.email);
+      var is_groupie = false;
+      for (var i=0; i<docs.groupies.length; i++) {
+        if (docs.groupies[i].email == user_email) {
+          is_groupie = true;
+        }
+      }
+      res.render(view, { "order": docs, "is_owner": is_owner, "is_groupie": is_groupie });
   });
 }
